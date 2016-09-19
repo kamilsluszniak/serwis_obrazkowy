4c2fd84498279de9389225756ff02449e22eb1cc
kamil_sluszniak <sultanaghmed@gmail.com>
2016-09-19 10:55:40 +0000

    poprawa przyciskow edycji

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,7 +7,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'posts/index'
-    assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
   end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -10,7 +10,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with valid information" do
     get login_path
-    post login_path, params: { session: { email: @user.email, password: 'password' } }
+    post login_path, params: { session: { email: @user.email, password: 'password'} }
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
