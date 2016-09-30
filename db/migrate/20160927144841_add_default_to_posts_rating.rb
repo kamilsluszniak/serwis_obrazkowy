c379dc3fa62591f5772f25bcbbd71cfbe497f43f
kamil_sluszniak <sultanaghmed@gmail.com>
2016-09-30 13:18:25 +0000

    pg bledy

--- db/migrate/20160927144841_add_default_to_posts_rating.rb
+++
@@ -1,9 +0,0 @@
-class AddDefaultToPostsRating < ActiveRecord::Migration[5.0]
-  def change
-    change_column :posts, :rating, :integer, :default => 0
-  end
-  
-
-    
-
-end
--- db/migrate/20160927145829_add_default_to_users_voted.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultToUsersVoted < ActiveRecord::Migration[5.0]
-  def change
-    add_column :posts, :users_voted, :text
-  end
-end
