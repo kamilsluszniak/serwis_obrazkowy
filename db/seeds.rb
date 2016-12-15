ba40c033b989ddf873da62d44e650d6138a96ae9
kamil_sluszniak <sultanaghmed@gmail.com>
2016-12-15 20:20:45 +0000

    poprawa html

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ User.create!(name:  "Example User",
              activated_at: Time.zone.now)
 
 
-99.times do |n|
+30.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
