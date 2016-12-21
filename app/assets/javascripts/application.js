ce9094f4816b59bd7640ebceb60c12d40a84a916
kamil_sluszniak <sultanaghmed@gmail.com>
2016-12-21 09:13:57 +0000

    drobne poprawki

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,5 +16,9 @@
 //= require_tree .
 //= require bootstrap
 
-
-
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+tag.id = "iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        
